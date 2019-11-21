54f63d1bc1922cc7a8666d3a240a5a63daff4f42
MaksP <maks2796@gmail.com>
2019-11-21 10:21:31 +0100

    Build project

---
+++ aplikacja_python/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\maks\desktop\aplikacje www\aplikacja_www\aplikacja_python\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
